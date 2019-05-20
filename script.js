d909890510195c093f53098ade9b8a751b50a2b8
Brandon Rodriguez <43117764+BrandonRodriguezC@users.noreply.github.com>
2019-05-20 13:01:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,120 @@
-let time = 0;
-let wave = [];
-let fourierX = [];
-let fourierY = [];
-let estado = false;
-let vectorX = [];
-let vectorY = [];
+const USER = 0;
+const FOURIER = 1;
 
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  //  fourier=transformadaFourierDiscreta(prueba);
-}
+let x = [];
+let y = [];
+let fourierX;
+let fourierY;
+let time = 0;
+let path = [];
+let drawing = [];
+let state = -1;
 
 function mousePressed() {
-  estado = false;
-  vectorX = [];
-  vectorY = [];
+  state = USER;
+  drawing = [];
+  x = [];
+  y = [];
+  time = 0;
+  path = [];
 }
 
 function mouseReleased() {
-  estado = true;
-  fourierX = transformadaFourierDiscreta(vectorX);
-  fourierY = transformadaFourierDiscreta(vectorX);
+  state = FOURIER;
+  const skip = 1;
+  for (let i = 0; i < drawing.length; i += skip) {
+    x.push(drawing[i].x);
+    y.push(drawing[i].y);
+  }
+  fourierX = dft(x);
+  fourierY = dft(y);
+
 }
 
-function draw() {
-  background(255, 204, 0);
-
-  if (estado == false) {
-    vectorX.push(mouseX);
-    vectorY.push(mouseY);
-
-  } else {
-
-    let puntosX = circulos(100, 100, 0,fourierX);
-    let puntosY = circulos(100, 100, HALF_PI, fourierY);
-    let puntos = createVector(puntosX.x, puntosY.y);
-    wave.unshift(puntos);
-    translate(200, 0);
-
-    beginShape();
-    noFill();
-    for (let i = 0; i < wave.length; i++) {
-      vertex(wave[i].x, wave[i].y);
-    }
-    endShape();
-
-    time += TWO_PI / fourierY.length;
-
-    if (time > TWO_PI) {
-      time = 0;
-      wave = [];
-  }
-  }
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  createP("Draw something!");
 }
 
-function circulos(x, y, rotacion,fourier) {
-
+function epiCycles(x, y, rotation, fourier) {
   for (let i = 0; i < fourier.length; i++) {
-    let xAnterior = x;
-    let yAnterior = y;
-
-    let radio = fourier[i].amplitud;
-    let periodo = fourier[i].periodo;
-    let angulo = fourier[i].angulo;
-    x += radio* cos(periodo * time + angulo+rotacion);
-    y += radio * sin(periodo * time + angulo+rotacion);
+    let prevx = x;
+    let prevy = y;
+    let freq = fourier[i].freq;
+    let radius = fourier[i].amp;
+    let phase = fourier[i].phase;
+    x += radius * cos(freq * time + phase + rotation);
+    y += radius * sin(freq * time + phase + rotation);
 
     stroke(0, 100);
     noFill();
-    ellipse(xAnterior, yAnterior, radio * 2);
-
-    fill(0);
+    ellipse(prevx, prevy, radius * 2);
     stroke(0);
-    line(xAnterior, yAnterior, x, y);
-    ellipse(x, y, 8);
+    line(prevx, prevy, x, y);
   }
   return createVector(x, y);
 }
 
+function draw() {
+  background(255,204,0);
 
-function transformadaFourierDiscreta(x) {
-  var X = [];
+  if (state == USER) {
+    let point = createVector(mouseX - width / 2, mouseY - height / 2);
+    drawing.push(point);
+    stroke(0);
+    noFill();
+    beginShape();
+    for (let v of drawing) {
+      vertex(v.x + width / 2, v.y + height / 2);
+    }
+    endShape();
+  } else if (state == FOURIER) {
+    let vx = epiCycles(width / 2, 100, 0, fourierX);
+    let vy = epiCycles(100, height / 2, HALF_PI, fourierY);
+    let v = createVector(vx.x, vy.y);
+    path.unshift(v);
+    line(vx.x, vx.y, v.x, v.y);
+    line(vy.x, vy.y, v.x, v.y);
+
+    beginShape();
+    noFill();
+    for (let i = 0; i < path.length; i++) {
+      vertex(path[i].x, path[i].y);
+    }
+    endShape();
+
+    const dt = TWO_PI / fourierY.length;
+    time += dt;
+
+    if (time > TWO_PI) {
+      time = 0;
+      path = [];
+    }
+  }
+
+  // if (wave.length > 250) {
+  //   wave.pop();
+  // }
+}
+
+function dft(x) {
+  const X = [];
   const N = x.length;
   for (let k = 0; k < N; k++) {
-    var real = 0;
-    var imaginaria = 0;
+    let re = 0;
+    let im = 0;
     for (let n = 0; n < N; n++) {
-      var tetha = (TWO_PI * k * n) / N;
-      real += x[n] * cos(tetha);
-      imaginaria -= x[n] * sin(tetha);
+      const phi = (TWO_PI * k * n) / N;
+      re += x[n] * cos(phi);
+      im -= x[n] * sin(phi);
     }
-    real = real / N;
-    imaginaria = imaginaria / N;
-    var periodo = k;
-    var amplitud = sqrt(real * real + imaginaria * imaginaria);
-    var angulo = atan2(imaginaria, real);
-    X[k] = {
-      real,
-      imaginaria,
-      periodo,
-      amplitud,
-      angulo
-    };
+    re = re / N;
+    im = im / N;
+
+    let freq = k;
+    let amp = sqrt(re * re + im * im);
+    let phase = atan2(im, re);
+    X[k] = { re, im, freq, amp, phase };
   }
   return X;
 }
